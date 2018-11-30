e8bcf87ddc0d657584014d33539dfaa5a91dfeee
Lagger <franek616@gmail.com>
2018-11-30 19:34:27 +0100

    #1 inicjalizacja projektu

---
+++ app/src/main/java/com/example/pk/wifinotes/NetworksActivity.java
@@ -0,0 +1,13 @@
+package com.example.pk.wifinotes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NetworksActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_networks);
+    }
+}
