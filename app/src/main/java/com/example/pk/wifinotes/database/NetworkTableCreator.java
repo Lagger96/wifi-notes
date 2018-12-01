88e6e96c19edc55a059e579bdb07880adc822a2d
ilinoyer <tmjaskowiec@gmail.com>
2018-12-01 11:22:26 +0100

    #2 Modele + data manager + dodane przykładowe dane do db dodawane przy tworzeniu schematu

--- app/src/main/java/com/example/pk/wifinotes/database/NetworkTableCreator.java
+++ app/src/main/java/com/example/pk/wifinotes/database/NetworkTableCreator.java
@@ -9,7 +9,17 @@ public class NetworkTableCreator {
     }
 
     public static void onCreate(SQLiteDatabase sqLiteDatabase) {
-        sqLiteDatabase.execSQL("CREATE TABLE NETOWORK (NETWORK_ID " + SQLiteDataTypes.INTEGER + " PRIMARY KEY, SSID " + SQLiteDataTypes.TEXT
+        sqLiteDatabase.execSQL("CREATE TABLE NETOWORK (NETWORK_ID " + SQLiteDataTypes.INTEGER + " PRIMARY KEY AUTOINCREMENT, SSID " + SQLiteDataTypes.TEXT
                 + ", PASSWORD " + SQLiteDataTypes.TEXT + ", DESCRIPTION " + SQLiteDataTypes.TEXT + ", CATEGORY " + SQLiteDataTypes.TEXT + ");");
+        insertTestData(sqLiteDatabase);
+    }
+
+    private static void insertTestData(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL("INSERT INTO NETWORK VALUES ('SIEC1', '123', 'Siec pierwsza', 'Categoria1')");
+        sqLiteDatabase.execSQL("INSERT INTO NETWORK VALUES ('SIEC2', '123', 'Siec druga', 'Categoria1')");
+        sqLiteDatabase.execSQL("INSERT INTO NETWORK VALUES ('SIEC3', '123', 'Siec trzecia', 'Categoria2')");
+        sqLiteDatabase.execSQL("INSERT INTO NETWORK VALUES ('SIEC4', '123', 'Siec czwarta', 'Categoria2')");
+        sqLiteDatabase.execSQL("INSERT INTO NETWORK VALUES ('SIEC5', '123', 'Siec piąta', 'Categoria3')");
+        sqLiteDatabase.execSQL("INSERT INTO NETWORK VALUES ('SIEC6', '123', 'Siec szósta', 'Categoria3')");
     }
 }
