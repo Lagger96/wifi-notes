d75df17ef24b23067c7f2a7c7fcb4fa1822b02f8
Touhou-fan <30899927+Touhou-fan@users.noreply.github.com>
2018-12-02 20:58:45 +0100

    #11 poprawienie paddingu przycisków

--- app/src/main/java/com/example/pk/wifinotes/AddNetworkDialog.java
+++ app/src/main/java/com/example/pk/wifinotes/AddNetworkDialog.java
@@ -1,6 +1,6 @@
 package com.example.pk.wifinotes;
 
-import android.app.AlertDialog;
+import android.support.v7.app.AlertDialog;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
