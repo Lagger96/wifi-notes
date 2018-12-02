553f8e3721e0da1df9244edbb7c8dfb986230847
Touhou-fan <30899927+Touhou-fan@users.noreply.github.com>
2018-12-02 21:35:31 +0100

    #11 zapewnij, że ssid i hasło są wypełnione

--- app/src/main/java/com/example/pk/wifinotes/AdderNetworkDialog.java
+++ app/src/main/java/com/example/pk/wifinotes/AdderNetworkDialog.java
@@ -1,5 +1,6 @@
 package com.example.pk.wifinotes;
 
+import android.content.DialogInterface;
 import android.support.v7.app.AlertDialog;
 import android.content.Context;
 import android.view.LayoutInflater;
@@ -19,7 +20,8 @@ public class AdderNetworkDialog extends AlertDialog {
         View layout = layoutInflater.inflate(R.layout.dialog_network_adder, null);
         setView(layout);
 
-        setButton(BUTTON_POSITIVE, context.getString(R.string.add), (dialog, which) -> {
+        setButton(BUTTON_POSITIVE, context.getString(R.string.add), (dialog, which) -> {});
+        setOnShowListener(dialog -> getButton(BUTTON_POSITIVE).setOnClickListener(view -> {
             EditText ssidText = layout.findViewById(R.id.ssid);
             EditText passwordText = layout.findViewById(R.id.password);
             EditText categoryText = layout.findViewById(R.id.category);
@@ -30,15 +32,22 @@ public class AdderNetworkDialog extends AlertDialog {
             String category = categoryText.getText().toString();
             String description = descriptionText.getText().toString();
 
+            System.out.println(ssid + " -> " + password);
+            if (ssid.equals("") || password.equals("")) {
+                Toast.makeText(context, context.getString(R.string.ssid_or_password_empty), Toast.LENGTH_SHORT).show();
+                return;
+            }
+
             Network network = new Network(null, ssid, password, description, category);
 
             DataManager dataManager = new DataManager(DbHelper.getInstance(getContext()).getWritableDatabase());
             if(dataManager.addNetwork(network)) {
                 Toast.makeText(context, context.getString(R.string.network_save_successful), Toast.LENGTH_SHORT).show();
+                dismiss();
             } else {
                 Toast.makeText(context, context.getString(R.string.network_save_failure), Toast.LENGTH_SHORT).show();
             }
-        });
+        }));
         setButton(BUTTON_NEGATIVE, context.getString(R.string.cancel), (dialog, which) -> {});
     }
 }
