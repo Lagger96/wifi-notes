104065c9c7abc8346a41e5521cb7dd36b09249e2
Touhou-fan <30899927+Touhou-fan@users.noreply.github.com>
2018-12-02 01:05:27 +0100

    dodaj zapisywanie wprowadzonych danych do bazy

--- app/src/main/java/com/example/pk/wifinotes/AddNetworkDialog.java
+++ app/src/main/java/com/example/pk/wifinotes/AddNetworkDialog.java
@@ -4,7 +4,10 @@ import android.app.AlertDialog;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
+import com.example.pk.wifinotes.database.DbHelper;
+import com.example.pk.wifinotes.models.Network;
 
 public class AddNetworkDialog extends AlertDialog {
 
@@ -16,14 +19,26 @@ public class AddNetworkDialog extends AlertDialog {
         View layout = layoutInflater.inflate(R.layout.add_network_dialog, null);
         setView(layout);
 
-        layout.findViewById(R.id.ssid);
-        layout.findViewById(R.id.password);
-        layout.findViewById(R.id.category);
-        layout.findViewById(R.id.description);
+        setButton(BUTTON_POSITIVE, context.getString(R.string.add), (dialog, which) -> {
+            EditText ssidText = layout.findViewById(R.id.ssid);
+            EditText passwordText = layout.findViewById(R.id.password);
+            EditText categoryText = layout.findViewById(R.id.category);
+            EditText descriptionText = layout.findViewById(R.id.description);
 
-        setButton(BUTTON_POSITIVE, context.getString(R.string.add),
-                (dialog, which) -> Toast.makeText(context, "ADD", Toast.LENGTH_SHORT).show());
-        setButton(BUTTON_NEGATIVE, context.getString(R.string.cancel),
-                (dialog, which) -> Toast.makeText(context, "CANCEL", Toast.LENGTH_SHORT).show());
+            String ssid = ssidText.getText().toString();
+            String password = passwordText.getText().toString();
+            String category = categoryText.getText().toString();
+            String description = descriptionText.getText().toString();
+
+            Network network = new Network(null, ssid, password, description, category);
+
+            DataManager dataManager = new DataManager(DbHelper.getInstance(getContext()).getWritableDatabase());
+            if(dataManager.addNetwork(network)) {
+                Toast.makeText(context, context.getString(R.string.network_save_successful), Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(context, context.getString(R.string.network_save_failure), Toast.LENGTH_SHORT).show();
+            }
+        });
+        setButton(BUTTON_NEGATIVE, context.getString(R.string.cancel), (dialog, which) -> {});
     }
 }
