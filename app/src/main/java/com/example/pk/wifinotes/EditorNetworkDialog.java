96c0219ea657aec6e7e69cc9305e33763487230b
Touhou-fan <30899927+Touhou-fan@users.noreply.github.com>
2018-12-03 17:19:57 +0100

    #13 dodanie usuwania

--- app/src/main/java/com/example/pk/wifinotes/EditorNetworkDialog.java
+++ app/src/main/java/com/example/pk/wifinotes/EditorNetworkDialog.java
@@ -34,27 +34,44 @@ public class EditorNetworkDialog extends AlertDialog {
         ssidText.setFocusable(false);
 
         setButton(BUTTON_POSITIVE, context.getString(R.string.edit), (dialog, which) -> {});
-        setOnShowListener(dialog -> getButton(BUTTON_POSITIVE).setOnClickListener(view -> {
-            int id = network.getId();
-            String ssid = network.getSsid();
-            String password = passwordText.getText().toString();
-            String category = categoryText.getText().toString();
-            String description = descriptionText.getText().toString();
+        setOnShowListener(dialog -> {
+            getButton(BUTTON_POSITIVE).setOnClickListener(view -> {
+                int id = network.getId();
+                String ssid = network.getSsid();
+                String password = passwordText.getText().toString();
+                String category = categoryText.getText().toString();
+                String description = descriptionText.getText().toString();
 
-            if (password.equals("")) {
-                Toast.makeText(context, context.getString(R.string.password_empty), Toast.LENGTH_SHORT).show();
-                return;
-            }
+                if (password.equals("")) {
+                    Toast.makeText(context, context.getString(R.string.password_empty), Toast.LENGTH_SHORT).show();
+                    return;
+                }
 
-            Network modifiedNetwork = new Network(id, ssid, password, description, category);
+                Network modifiedNetwork = new Network(id, ssid, password, description, category);
 
-            DataManager dataManager = new DataManager(DbHelper.getInstance(getContext()).getWritableDatabase());
-            if(dataManager.updateNetwork(modifiedNetwork)) {
-                Toast.makeText(context, context.getString(R.string.network_edit_successful), Toast.LENGTH_SHORT).show();
-                //callback.run();
-                dismiss();
-            }
-        }));
+                DataManager dataManager = new DataManager(DbHelper.getInstance(getContext()).getWritableDatabase());
+                if (dataManager.updateNetwork(modifiedNetwork)) {
+                    Toast.makeText(context, context.getString(R.string.network_edit_successful), Toast.LENGTH_SHORT).show();
+                    // TODO refresh
+                    dismiss();
+                }
+            });
+            getButton(BUTTON_NEUTRAL).setOnClickListener(view -> {
+                AlertDialog.Builder builder = new AlertDialog.Builder(context);
+                AlertDialog alertDialog = builder
+                        .setMessage(String.format(context.getString(R.string.delete_confirmation), network.getSsid()))
+                        .setPositiveButton(context.getString(R.string.yes), (dialog1, which) -> {
+                            DataManager dataManager = new DataManager(DbHelper.getInstance(getContext()).getWritableDatabase());
+                            dataManager.deleteNetwork(network.getId());
+                            // TODO close edit dialog
+                            // TODO refresh
+                        })
+                        .setNegativeButton(context.getString(R.string.no), (dialog12, which) -> {})
+                        .create();
+                alertDialog.show();
+            });
+        });
         setButton(BUTTON_NEGATIVE, context.getString(R.string.cancel), (dialog, which) -> {});
+        setButton(BUTTON_NEUTRAL, context.getString(R.string.delete), (dialog, which) -> {});
     }
 }
