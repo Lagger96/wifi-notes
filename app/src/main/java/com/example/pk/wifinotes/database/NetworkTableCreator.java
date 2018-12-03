e853a9bc35dc93a712d602a2be2c463c2c2331c9
Touhou-fan <30899927+Touhou-fan@users.noreply.github.com>
2018-12-03 21:06:47 +0100

    #13 usunięcie testowych danych z bazy

--- app/src/main/java/com/example/pk/wifinotes/database/NetworkTableCreator.java
+++ app/src/main/java/com/example/pk/wifinotes/database/NetworkTableCreator.java
@@ -9,17 +9,9 @@ public class NetworkTableCreator {
     }
 
     public static void onCreate(SQLiteDatabase sqLiteDatabase) {
-        sqLiteDatabase.execSQL("CREATE TABLE NETWORK (NETWORK_ID " + SQLiteDataTypes.INTEGER + " PRIMARY KEY AUTOINCREMENT, SSID " + SQLiteDataTypes.TEXT
-                + " UNIQUE, PASSWORD " + SQLiteDataTypes.TEXT + ", DESCRIPTION " + SQLiteDataTypes.TEXT + ", CATEGORY " + SQLiteDataTypes.TEXT + ");");
-        insertTestData(sqLiteDatabase);
-    }
-
-    private static void insertTestData(SQLiteDatabase sqLiteDatabase) {
-        sqLiteDatabase.execSQL("INSERT INTO NETWORK (SSID, PASSWORD , DESCRIPTION , CATEGORY ) VALUES ('SIEC1', '123', 'Siec pierwsza', 'Kategoria1')");
-        sqLiteDatabase.execSQL("INSERT INTO NETWORK (SSID, PASSWORD , DESCRIPTION , CATEGORY ) VALUES ('SIEC2', '123', 'Siec druga', 'Kategoria1')");
-        sqLiteDatabase.execSQL("INSERT INTO NETWORK (SSID, PASSWORD , DESCRIPTION , CATEGORY ) VALUES ('SIEC3', '123', 'Siec trzecia', 'Kategoria2')");
-        sqLiteDatabase.execSQL("INSERT INTO NETWORK (SSID, PASSWORD , DESCRIPTION , CATEGORY ) VALUES ('SIEC4', '123', 'Siec czwarta', 'Kategoria2')");
-        sqLiteDatabase.execSQL("INSERT INTO NETWORK (SSID, PASSWORD , DESCRIPTION , CATEGORY ) VALUES ('SIEC5', '123', 'Siec piąta', 'Kategoria3')");
-        sqLiteDatabase.execSQL("INSERT INTO NETWORK (SSID, PASSWORD , DESCRIPTION , CATEGORY ) VALUES ('SIEC6', '123', 'Siec szósta', 'Kategoria3')");
+        sqLiteDatabase.execSQL("CREATE TABLE NETWORK (NETWORK_ID " + SQLiteDataTypes.INTEGER
+                + " PRIMARY KEY AUTOINCREMENT, SSID " + SQLiteDataTypes.TEXT + " UNIQUE, PASSWORD "
+                + SQLiteDataTypes.TEXT + ", DESCRIPTION " + SQLiteDataTypes.TEXT + ", CATEGORY "
+                + SQLiteDataTypes.TEXT + ");");
     }
 }
