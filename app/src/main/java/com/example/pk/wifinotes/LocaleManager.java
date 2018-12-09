4b7ea04a8090c1686255eee8fe1033062b1e9edf
ilinoyer <tmjaskowiec@gmail.com>
2018-12-09 21:13:28 +0100

    #27 możliwość przełączania jęzka # dodana możliwość przełączania jezyka

---
+++ app/src/main/java/com/example/pk/wifinotes/LocaleManager.java
@@ -0,0 +1,50 @@
+package com.example.pk.wifinotes;
+
+import android.content.Context;
+import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.preference.PreferenceManager;
+
+import java.util.Locale;
+
+public class LocaleManager {
+
+    private static final String LANGUAGE_ENGLISH = "en";
+    private static final String LANGUAGE_POLISH = "pl";
+    private static final String LANGUAGE_KEY = "language";
+
+    public static Context setLocale(Context c) {
+        return updateResources(c, getLanguage(c));
+    }
+
+    public static void changeLocale(Context c) {
+        if(getLanguage(c).equalsIgnoreCase(LANGUAGE_ENGLISH)) {
+            persistLanguage(c, LANGUAGE_POLISH);
+            updateResources(c, LANGUAGE_POLISH);
+        } else {
+            persistLanguage(c, LANGUAGE_ENGLISH);
+            updateResources(c, LANGUAGE_ENGLISH);
+        }
+    }
+
+    private static String getLanguage(Context context) {
+        return PreferenceManager.getDefaultSharedPreferences(context).getString(LANGUAGE_KEY, LANGUAGE_ENGLISH);
+    }
+
+    private static void persistLanguage(Context context, String language) {
+        PreferenceManager.getDefaultSharedPreferences(context).edit().putString(LANGUAGE_KEY, language).apply();
+    }
+
+    private static Context updateResources(Context context, String language) {
+        Locale locale = new Locale(language);
+        Locale.setDefault(locale);
+
+        Resources res = context.getResources();
+        Configuration config = new Configuration(res.getConfiguration());
+        config.setLocale(locale);
+
+        context = context.createConfigurationContext(config);
+
+        return context;
+    }
+}
--- app/src/main/java/com/example/pk/wifinotes/NetworksActivity.java
+++ app/src/main/java/com/example/pk/wifinotes/NetworksActivity.java
@@ -1,5 +1,6 @@
 package com.example.pk.wifinotes;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
@@ -32,6 +33,7 @@ public class NetworksActivity extends AppCompatActivity {
 
         setupAddButton();
         setupImportButton();
+        setupLanguageButton();
 
         ViewPager viewPager = findViewById(R.id.viewpager);
         setupViewPager(viewPager);
@@ -40,6 +42,11 @@ public class NetworksActivity extends AppCompatActivity {
         tabLayout.setupWithViewPager(viewPager);
 
         dataManager = new DataManager(DbHelper.getInstance(this).getWritableDatabase());
+    }
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(LocaleManager.setLocale(base));
     }
 
     private void setupImportButton() {
@@ -59,6 +66,16 @@ public class NetworksActivity extends AppCompatActivity {
     private void addNetwork() {
         AdderNetworkDialog adderNetworkDialog = new AdderNetworkDialog(this, this::refreshViews);
         adderNetworkDialog.show();
+    }
+
+    private void setupLanguageButton() {
+        Button languageButton = findViewById(R.id.button_language);
+        languageButton.setOnClickListener(view -> changeLanguage());
+    }
+
+    private void changeLanguage() {
+        LocaleManager.changeLocale(this);
+        recreate();
     }
 
     private void setupViewPager(ViewPager viewPager) {
